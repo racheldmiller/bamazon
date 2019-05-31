4542cae0fc0c5241b8ec464ee8b1706afcfa023a
racheldmiller <rachelmiller415@gmail.com>
2019-05-31 08:40:38 -0700

    added more functions

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -14,15 +14,81 @@ var connection = mysql.createConnection({
   database: "bamazon_db"
 });
 
+// App first displays all of the items available for sale.
 connection.connect(function(err) {
   if (err) throw err;
+  console.log("connected as id " + connection.threadId + "\n");
+  itemsForSale();
 });
 
-// Running this application will first display all of the items available for sale.
 // Include the ids, names, and prices of products for sale.
+function itemsForSale() {
+  console.log("Want to buy these?");
+  connection.query("SELECT * FROM products", function(err, res) {
+    if (err) {
+      throw err;
+    } else {
+      console.table(res);
+      buyById(res); // placement here for asynchronicity
+    }
+  });
+}
+
+function buyById(inventory) {
+  inquirer
+    .prompt({
+      name: "item_id",
+      type: "input",
+      message: "Pick the ID of the products you'd like to buy"
+    })
+    .then(function(answer) {
+      var choiceId = parseInt(answer.item_id);
+      var product = checkInventory(inventory, choiceId);
+      if (product) {
+        // console.log("works");
+        quantity(product);
+      } else {
+        console.log("product not found");
+      }
+    });
+}
+
+function checkInventory(inventory, choiceId) {
+  // sub "val" for "inventory"
+  for (var i = 0; i < inventory.length; i++) {
+    if (choiceId === inventory[i].item_id) {
+      // console.log(inventory[i]);
+      return inventory[i];
+    }
+  }
+  return null;
+}
+
+function quantity(product) {
+  inquirer
+    .prompt({
+      name: "quantity",
+      type: "input",
+      message: "How many units would you like to buy?"
+    })
+    .then(function(answer) {
+      var amount = parseInt(answer.quantity);
+      // console.log(amount);
+      if (amount > product.stock_quantity) {
+        console.log("Insufficient quantity!");
+        itemsForSale();
+      }
+    });
+}
+
+// next steps
+// function toBuy (to make an actual purchase)
+// update sql query
+
 // The app should then prompt users with two messages.
-// The first should ask them the ID of the product they would like to buy.
-// The second message should ask how many units of the product they would like to buy.
+// 1st: ID of product they want to buy.
+// 2nd: how many units of product they'd want to buy.
+
 // Once the customer has placed the order, your application should check if your store has enough
 // of the product to meet the customer's request.
 // If not, the app should log a phrase like Insufficient quantity!, and then prevent the order from going through.
